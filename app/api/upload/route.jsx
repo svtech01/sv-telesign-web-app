63ca1434d34ed55c8cc875624144872730d996a1
sv-bpaltezo <bry@scalevirtually.com>
2025-11-07 04:04:50 +0800

    dynamic api url

--- app/api/upload/route.jsx
+++ app/api/upload/route.jsx
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
 
-const api_urls = {
-  "local": "http://127.0.0.1:5000/api/",
-  "staging": "https://sv-telesign-api.vercel.app/api/",
-  "production": "https://sv-telesign-api.vercel.app/api/"
-}
+// const api_urls = {
+//   "local": "http://127.0.0.1:5000/api/",
+//   "staging": "https://sv-telesign-api.vercel.app/api/",
+//   "production": "https://sv-telesign-api.vercel.app/api/"
+// }
 
-const ENV = "staging";
-const API_URL = api_urls[ENV];
+const API_URL = process.env.API_URL;
 
 export async function POST(req) {
   const formData = await req.formData();
