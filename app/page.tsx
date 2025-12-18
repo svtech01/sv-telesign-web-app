3aed9edb051ebe9004abae67d59a121929030f79
sv-bpaltezo <bry@scalevirtually.com>
2025-12-19 02:07:41 +0800

    application status from env

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import ValidationForm from "./components/ValidationForm.jsx";
 
 export default function Home() {
+
+  const accessibility = process.env.APPLICATION_STATUS || 'disabled';
+
+  if (accessibility !== 'enabled') {
+    return (
+      <main className="min-h-screen bg-gray-50 flex items-center justify-center" style={{width: '100%', background: "#dee2e1ff"}}>
+        <p className="text-xl font-bold text-orange-400">Application is currently disabled.</p>
+      </main>
+    );
+  }
+
   return (
     <main className="min-h-screen bg-gray-50 flex items-center justify-center" style={{width: '100%', background: "#dee2e1ff"}}>
       <ValidationForm />
